ba621c376531ce32eaaa9c66c0a456f807cec374
Fer03Liz <fjlizarazog@gmail.com>
2025-10-01 22:38:26 -0500

    Add SOAP endpoint and request/response DTOs for payment processing

---
+++ demo/src/main/java/com/taller3arqui/demo/DTO/CrearPagoResponse.java
@@ -0,0 +1,8 @@
+package com.taller3arqui.demo.DTO;
+
+public class CrearPagoResponse {
+    private String resultado;
+
+    public String getResultado() { return resultado; }
+    public void setResultado(String resultado) { this.resultado = resultado; }
+}
